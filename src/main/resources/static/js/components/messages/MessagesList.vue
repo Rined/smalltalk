58a4d1155d1bbe721f9bdf704f02005c91337cc7
Anton Petrov <antoxa-ac@list.ru>
2020-04-04 21:17:03 +0300

    gitignore

---
+++ src/main/resources/static/js/components/messages/MessagesList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="position: relative; width: 300px">
+        <!--
+                <message-from :messages="messages" :messageAttr="message"/>
+                <message-row v-for="message in messages"
+                             :key="message.id"
+                             :message="message"
+                             :editMethod="editMethod"
+                             :messages="messages"/> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['messages'],
+        data() {
+            return {
+                message: null
+            }
+        },
+        methods: {
+            editMethod(message) {
+                this.message = message
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
