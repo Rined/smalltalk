1bea03df431d85dc35b2da208c3119fd4a853995
Anton Petrov <antoxa-ac@list.ru>
2020-04-04 21:17:37 +0300

    webpack; vue

--- src/main/java/com/rined/smalltalk/controller/MainController.java
+++ src/main/java/com/rined/smalltalk/controller/MainController.java
@@ -3,6 +3,7 @@ package com.rined.smalltalk.controller;
 import com.rined.smalltalk.domain.User;
 import com.rined.smalltalk.services.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,12 +18,16 @@ import java.util.HashMap;
 public class MainController {
     private final MessageService messageService;
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user) {
         HashMap<Object, Object> data = new HashMap<>();
         data.put("profile", user);
         data.put("messages", messageService.findAll());
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
         return "index";
     }
 
